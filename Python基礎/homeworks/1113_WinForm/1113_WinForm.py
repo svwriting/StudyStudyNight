ad65656f9c6e01fdf7b86a6a8332fdcc275e2ed5
svwriting <70704878+svwriting@users.noreply.github.com>
2020-11-13 14:55:34 +0800

    Update 1113_WinForm.py

--- Python基礎/homeworks/1113_WinForm/1113_WinForm.py
+++ Python基礎/homeworks/1113_WinForm/1113_WinForm.py
@@ -115,7 +115,7 @@ class MainFrame ( wx.Frame ):
 			txtfilepath_=wx.FileSelector(
 				"儲存檔案",
 				default_filename=name_,
-				wildcard=name_+"txt",
+				wildcard=".txt",
 				flags=wx.FD_SAVE
 			)
 			if txtfilepath_:
@@ -144,7 +144,7 @@ class MainFrame ( wx.Frame ):
 		exit()
 		pass
 
-	def textchange( self, event ): #結束程式
+	def textchange( self, event ): 
 		if self.isedited==False:
 			self.Title='*'+self.Title
 			self.isedited=True
