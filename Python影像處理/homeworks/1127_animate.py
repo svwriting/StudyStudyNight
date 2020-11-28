139a199f3f1cf6c4e667d55999298c1b46bd0b37
svwriting <70704878+svwriting@users.noreply.github.com>
2020-11-28 15:01:59 +0800

    Add files via upload

--- Python影像處理/homeworks/1127_animate.py
+++ Python影像處理/homeworks/1127_animate.py
@@ -1,22 +1,22 @@
 import cv2 ,os 
 import numpy as np
 
-def PSmerge(frameN):
+# 速度想調成一樣太麻煩了，我放棄
+# 這和距離增減值、等待秒數、還有程式碼效率有關
+# 而距離增減值和程式碼效率也會影響動畫平滑度
+square_=np.full((100,100,3),(255,0,0),np.uint8)         # 正方形
+wholepic=np.full((300,500,3),(255,255,255),np.uint8)    # 畫布
+frameN=0                                                # 第幾幀
+d=2							# 距離增減值
+
+def PSmerge(frameN):					# 把正方形放到白色畫布上
     return cv2.rectangle(wholepic.copy(),(frameN,101),(100+frameN,200),(255,0,0),-1)
 
-square_=np.full((100,100,3),(255,0,0),np.uint8)
-#square_=cv2.rectangle(square_,(0,100),(100,100),(255,0,0),-1)
-wholepic=np.full((300,500,3),(255,255,255),np.uint8)
 
-cv2.imshow("image",wholepic)
-wholepic= np.vstack([wholepic,])
-KeyIn_=None
-frameN=0
-d=1
 while True:
     cv2.imshow("image",PSmerge(frameN))
     frameN+=d
-    if cv2.waitKey(5)!=-1:
+    if cv2.waitKey(1)!=-1:
         break
     if frameN<=0 or frameN>=400:
         d*=-1
