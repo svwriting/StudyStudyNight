0fb7a12c3559afa3a2afda59849b79e5de935902
svwriting <70704878+svwriting@users.noreply.github.com>
2020-11-28 15:27:58 +0800

    Add files via upload

--- Python影像處理/homeworks/1127_OnlyText.py
+++ Python影像處理/homeworks/1127_OnlyText.py
@@ -16,15 +16,20 @@ def method2(img_): # inRange
   img_=cv2.inRange(img_, (0,0,255), (0,0,255))
   return 255-img_
 
-def method3(img_): # cv2四則運算，但感覺效率待商榷
+def method3(img_): # cv2四則運算，但感覺這種搞法的意義不明
   R_=img_[:,:,2]
-  img_=cv2.add(img_[:,:,0]+img_[:,:,1],254)
-  img_=cv2.subtract(R_,img_)
-  img_=cv2.multiply(1-img_,255)
+  G_=img_[:,:,1]
+  B_=img_[:,:,0]
+  img_=R_
+  img_=cv2.subtract(img_,G_)    # 確定G_皆0，避免(1~254,1~254,255)
+  img_=cv2.subtract(img_,B_)    # 確定B_皆0，避免(1~254,1~254,255)
+  img_=cv2.subtract(img_,254)   # 確定R_皆255
+  img_=cv2.multiply(img_,255)   # 乘255(=做二值化Histogram)
+  img_=cv2.subtract(255,img_)   # 反白(=255-img=bitwise_not(img_))
   return img_
 
 # 我不懂他要的是什麼 OTZ...
 # cv2.imshow("img",method1(img.copy())) # 矩陣做，但老師說不要用迴圈和判斷
 # cv2.imshow("img",method2(img.copy())) # inRange，但老師說要+-*/
-cv2.imshow("img",method3(img.copy())) # 老師是要用cv2的四則運算？
+cv2.imshow("img",method3(img.copy())) # 老師是要用cv2的四則？但感覺這做法做了許多無用計算
 cv2.waitKey()
